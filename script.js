eaea8d7ab574ef8029ee9c9f95b8d7ad7768e368
Saurabh9907 <souravekumarparjapati@gmail.com>
2026-05-20 18:48:15 +0530

    Updated story builder validation and UI fixes

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function generateStory(){
 
-  localStorage.setItem("time", document.getElementById("timeInput").value);
-  localStorage.setItem("battery", document.getElementById("batteryInput").value);
   localStorage.setItem("username", document.getElementById("usernameInput").value);
   localStorage.setItem("storyTime", document.getElementById("storyTimeInput").value);
   localStorage.setItem("mainText", document.getElementById("mainTextInput").value);
+ 
 
   const file = document.getElementById("profileInput").files[0];
   const reader = new FileReader();
@@ -23,11 +22,6 @@ function generateStory(){
 
 if(window.location.pathname.includes("story.html")){
 
-  document.getElementById("displayTime").innerText =
-    localStorage.getItem("time");
-
-  document.getElementById("displayBattery").innerText =
-    localStorage.getItem("battery");
 
   document.getElementById("displayUsername").innerText =
     localStorage.getItem("username");
